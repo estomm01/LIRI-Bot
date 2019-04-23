072ed6dbb455ec7c3b8aff2c1b6def28cbe82e02
Markus Maasikas <markus@hotmail.com>
2019-04-22 21:36:19 -0400

    added to JS and other files

--- liri.js
+++ liri.js
@@ -1,74 +1,82 @@
-var spotify = require('spotify');
-var request = require('request');
-var fs = require('fs');
-var keys = require("./keys.js");
-var dotenv  = require("dotenv").config();
+require("dotenv").config();
+ let keys = require("./keys.js");
+ let inquirer = require("inquirer");
+ let Spotify = require('node-spotify-api');
+ let spotify = new Spotify(keys.spotify);
+ let axios = require("axios");
+ let moment = require('moment');
+
+// let Spotify = require('node-spotify-api');
+// let request = require('request');
+// let fs = require('fs');
+// let keys = require("./keys.js");
+// let dotenv = require("dotenv").config();
+// console.log(keys);
+
+// //moment js
+// let moment = require('moment');
 
 
-//moment js
-var moment = require('moment');
-moment().format();
-
-//spotify keys
-var spotify = new Spotify(keys.spotify);
-var omdbKey = keys.omdb.api_key;
+// //spotify keys
+// var spotify = new Spotify(keys.spotify);
+// var omdbKey = keys.omdb.api_key;
 
 //variable for input
 var command = process.argv[2];
 var input = process.argv[3];
 
-//This function processes the input commands
-function processCommands(command, commandParam){
 
-	//console.log(commandParam);
+//console.log(commandParam);
 
-	switch(command){
-    case 'spotify-this-song':
+switch (command) {
+	case 'spotify-this-song':
 		//If user has not specified a song , use default
-		if(commandParam === undefined){
+		if (commandParam === undefined) {
 			commandParam = "The Sing Ace of Base";
 		}
-    spotifyThis(commandParam); break;
+	 spotifyThis(commandParam); break;
+	default:
+		break;
+};
 
-		spotify.search({ type: 'track', query: commandParam }, function (err, data) {
-			if (err) {
-					return console.log('Error occurred: ' + err);
-			}
-			else {
-					for (i = 0; i < data.tracks.items.length; i++){
+function spotifyThis(song) {
+	spotify.search({ type: 'track', query: song }, function (err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		else {
+			for (var i = 0; i < data.tracks.items.length; i++) {
 
-							var musicQuery = data.tracks.items[i];
-							// console.log("===============================");
-							 // * Artist(s)
-							console.log("Artist" + musicQuery.artists[0].name);
-							//The song's name
-							console.log( "name" + musicQuery.name);
-							// preview link of the song from Spotify
-							console.log("preview URL" + musicQuery.preview_url);
-							//The album that the song is from
-							console.log("Album" + musicQuery.album.name);
-							console.log("-----------------");
-							};
-						};
-				});
+				var musicQuery = data.tracks.items[i];
+				// console.log("===============================");
+				// * Artist(s)
+				console.log("Artist" + musicQuery.artists[0].name);
+				//The song's name
+				console.log("name" + musicQuery.name);
+				// preview link of the song from Spotify
+				console.log("preview URL" + musicQuery.preview_url);
+				//The album that the song is from
+				console.log("Album" + musicQuery.album.name);
+				console.log("-----------------");
+			};
+		};
+	});
 
-	function omdb(movie){
+	// function omdb(movie){
 
-		var queryUrl = "http://www.omdbapi.com/?t" +
-
-
+	// 	var queryUrl = "http://www.omdbapi.com/?t" +
 }
 //Do what it says reads text from random.txt file, command is ran
-var doWhatItSays = function() {
+var doWhatItSays = function () {
 	fs.readFile("random.txt", "utf8", function (err, data) {
-			if (err) throw err;
-					var randomText = data.split(",");
+		if (err) throw err;
+		var randomText = data.split(",");
 
-			if (randomText.length == 2) {
-					ask(randomText[0], randomText[1]);
-			}
-			else if (randomText.length == 1) {
-					ask(randomText[0]);
-			}
+		if (randomText.length == 2) {
+			ask(randomText[0], randomText[1]);
+		}
+		else if (randomText.length == 1) {
+			ask(randomText[0]);
+		}
 	});
 }
