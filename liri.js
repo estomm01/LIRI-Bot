adb735c8a56dae5d91da0fc208d969096522b7e4
Markus Maasikas <markus@hotmail.com>
2019-04-24 21:18:18 -0400

    updated function movieThis

--- liri.js
+++ liri.js
@@ -81,21 +81,29 @@ function movieThis(movieQuery) {
 	var queryUrl = "http://www.omdbapi.com/?t=" + movieQuery + "&apikey=e97af0fa";
 	axios.get(queryUrl).then(function (results) {
 		//console.log(results.data.Year);
-	var movieData = results.data
+		if (err){
+			return console.log('Error occurred: ' + err);
 
-	console.log("Movie Title:" + movieData.Title);
-	console.log("Year the movie came out:"+ movieData.Year);
+//console.log(results.data.Year);
+var movieData = results.data
 
-	});
+console.log("Movie Title:" + movieData.Title);
+console.log("Year the movie came out:"+ movieData.Year);
+ console.log("IMDB Rating of the movie:" + movieData.Rating);
+ console.log("Country where the movie was produced:" + movieData.Country);
+ console.log("Language of the movie:" + movieData.Language);
+ console.log("Plot of the movie:" + movieData.Plot);
+ console.log("Actors in the movie:" + movieData.Actors);
+
+	 } else{
+				 console.log('Error occurred.')
+			 }
+			 if(movie === "Mr. Nobody"){
+				 console.log("-----------------------");
+				 console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+				 console.log("It's on Netflix!");
 };
-
-
-
-
-
-
-
-
+});
 
 
 
